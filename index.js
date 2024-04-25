fa088fe2e5901b2bf103f415ef5df69b7c7d2b82
muhammaduzair67 <161385100+muhammaduzair67@users.noreply.github.com>
2024-04-26 02:00:32 +0500

    adding a number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\twelcome to CodeWithUzair - CLI Number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "Enter your guess number",
+        message: "Enter your guess number(Number Limit from 1 to 5):",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("congratulation ! You Guess a correct number.");
+}
+else {
+    console.log("sorry,you guess a wrong number");
+}
